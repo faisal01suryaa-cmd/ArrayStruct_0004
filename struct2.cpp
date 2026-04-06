eb3a2d7d177dfbe125f49f18cb24be0d9f8a20bb
faisal01suryaa <faisal01suryaa@gmail.com>
2026-04-06 11:07:20 +0700

    menambahkan kota dan desa

--- struct2.cpp
+++ struct2.cpp
@@ -23,14 +23,17 @@ int main()
     // mengisi data
     cout << "Isikan data nama : ";
     cin >> mhs.nama;
-    cout << "Isikan data alamat : ";
-    cin >> mhs.alamat;
+    cout << "Isikan data kota : ";
+    cin >> mhs.alamat.kota;
+    cout << "Isikan data desa : ";
+    cin >> mhs.alamat.desa;
     cout << "Isikan data usia : ";
     cin >> mhs.umur;
 
     cout << "Data Mahasiswa" << endl;
     cout << "Nama : " << mhs.nama << endl;
-    cout << "Alamat : " << mhs.alamat << endl;
+    cout << "Alamat : " << mhs.alamat.kota << endl;
+     cout << "Alamat : " << mhs.alamat.desa << endl;
     cout << "Usia : " << mhs.umur << endl;
 
 }

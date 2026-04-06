29e18fc695ed3e5148476b1b94421a55722999da
faisal01suryaa <faisal01suryaa@gmail.com>
2026-04-06 10:51:29 +0700

    mengisikan data satu per satu

--- struct.cpp
+++ struct.cpp
@@ -7,3 +7,20 @@ struct mahasiswa
     string alamat;
     int umur;
 };
+
+int main()
+{
+    // deklarasi variable struct
+    mahasiswa mhs;
+
+    // mengisi data
+    cout << "Isikan data nama : ";
+    cin >> mhs.nama;
+    cout << "Isikan data alamat : ";
+    cin >> mhs.alamat;
+    cout << "Isikan data usia : ";
+    cin >> mhs.umur;
+
+    
+
+}

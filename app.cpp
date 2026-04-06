a63b6e3ebe9746c6641332bae24a53ef82a1e770
faisal01suryaa <faisal01suryaa@gmail.com>
2026-04-06 10:43:50 +0700

    menampilkan data dengan perulangan

--- app.cpp
+++ app.cpp
@@ -40,4 +40,10 @@ int main ()
         cin >> datamu[i];
     }
 
+     // menampilkan data dengan perulangan
+    for (int i = 0; i <= 4; i++)
+    {
+        cout << "Data ke-" << (i + 1) << " : " << datamu[i] << endl;
+    }
+
 }
